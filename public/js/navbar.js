f6cf5855c9dcdd0258e75ad12671be24eb8553e0
navid-al-azim <navidalazim95@gmail.com>
2024-02-06 13:23:04 +0600

    navbar updated

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -14,65 +14,70 @@ function loadNavbar() {
 </head>
 <body id="main-box">
     <div class="head-container">
-	<div class="logo-container"> <a href="/">
-        <div class="logo">
-            <img src="your-logo-image-path.png" alt="Your Logo">
-        </div>
-        <div class="website-name">
-            Kryptocasino
-        </a></div>
-    </div>
-    <div class="bar">
-        <ul>
-            <li><a href="#">Cryptocurrencies</a>
-                <div class="about">
-                    <ul>
-                        <li><a href="#">Bitcoin</a></li>
-                        <li><a href="#">Ethereum</a></li>
-                        <li><a href="#">Dogecoin</a></li>
-                        <li><a href="#">Litecoin</a></li>
-                        <li><a href="#">Binance Coin</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li><a href="#">Online Casinos</a>
-                <div class="about">
-                    <ul>
-                        <li><a href="#">New Online Casinos</a></li>
-                        <li><a href="#">VPN Casinos</a></li>
-                        <li><a href="#">Mobile Casinos</a></li>
-                        <li><a href="#">Casino Bonuses</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li><a href="#">Payment Methods</a>
-                <div class="things">
-                    <ul>
-                        <li><a href="#">Mastercard casinos</a></li>
-                        <li><a href="#">Visa Casinos</a></li>
-                        <li><a href="#">Neteller</a></li>
-                        <li><a href="#">Bank Transfer</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li><a href="#">Casino Games</a>
-                <div class="things">
-                    <ul>
-                        <li><a href="#">Slots</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li><a href="#">Providers</a>
-                <div class="things">
-                    <ul>
-                        <li><a href="#">Microgaming</a></li>
-                        <li><a href="#">Spinomenal</a></li>
-                        <li><a href="#">Push Gaming</a></li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
-    </div> <!--end of .bar--> </div>
+    <header>
+	<div class="container">
+	  <div class="nav-wrapper">
+		
+		<a href="/" class="logo">
+		  <img src="/images/logo.png" alt="CasinoCanada Logo">
+		</a>
+		<span class="site-name">#1 Casino Review Site - GamblingCentral </span>
+		<div class ="homebutton"><a href="/" style = "color: white;"> HOME </a></div>
+	  </div>
+	  <nav>
+		<ul>
+				<li>
+					<a href="#">Cryptocurrencies▾</a>
+					<div class="casino-online-submenu">
+					<ul>
+						<li><a href="/">Bitcoin</a></li>
+						<li><a href="/">Ethereum</a></li>
+						<li><a href="/">Dogecoin</a></li>
+                        <li><a href="/">Litecoin</a></li>
+                        <li><a href="/">Binance Coin</a></li>
+					</ul>
+					</div>
+				</li>
+		  <li><a href="#">Online Casinos▾</a>
+		  <div class="casino-online-submenu">
+			  <ul>
+              <li><a href="/">New Online Casinos</a></li>
+              <li><a href="/">VPN Casinos</a></li>
+              <li><a href="/">Mobile Casinos</a></li>
+              <li><a href="/">Casino Bonus</a></li>
+			  </ul>
+			</div></li>
+		  <li><a href="#">Payment Methods▾</a>
+		  <div class="casino-online-submenu">
+			  <ul>
+              <li><a href="/">Mastercard Casinos</a></li>
+              <li><a href="/">Visa Casinos</a></li>
+              <li><a href="/">Neteller</a></li>
+              <li><a href="/">Bank Transfer</a></li>
+			  </ul>
+			</div></li>
+		  
+		  <li><a href="#">Casino Games▾</a>
+		  <div class="casino-online-submenu">
+			  <ul>
+              <li><a href="/">Slots</a></li>
+			  </ul>
+			</div></li>
+		  <li><a href="#">Providers▾</a>
+		  <div class="casino-online-submenu">
+			  <ul>
+              <li><a href="/">Microgaming</a></li>
+              <li><a href="/">Spinomenal</a></li>
+              <li><a href="/">Push Gaming</a></li>
+				
+			  </ul>
+			</div></li>
+		  
+		</ul>
+	  </nav>
+	</div>
+  </header>
+	 </div>
 </body> <!--end of #main-box-->
 </html>
 
