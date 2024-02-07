6d356d185375573dba4f8e3366849f41c8431a98
navid-al-azim <navidalazim95@gmail.com>
2024-02-07 06:18:26 +0600

    site updated without the review pages

--- public/js/footer.js
+++ public/js/footer.js
@@ -19,32 +19,31 @@ function loadFooter() {
     <!-- Your page content goes here -->
 
     <div class="footer">
+    <div class="footer-column"></div>
         <div class="footer-column">
             <h2>Cryptocurrencies</h2>
             <ul>
-                <li><a href="/pages/1-currencies/bitcoin.html">Bitcoin</a></li>
-                <li><a href="/pages/1-currencies/ethereum.html">Ethereum</a></li>
-                <li><a href="/pages/1-currencies/doge.html">Dogecoin</a></li>
-                <li><a href="/pages/1-currencies/tether.html">Tether</a></li>
-                <li><a href="/pages/1-currencies/binance.html">Binance Coin</a></li>
+                <li><a href="/pages/1-currencies/bitcoin/">Bitcoin</a></li>
+                <li><a href="/pages/1-currencies/ethereum/">Ethereum</a></li>
+                <li><a href="/pages/1-currencies/dogecoin/">Dogecoin</a></li>
+                <li><a href="/pages/1-currencies/tether/">Tether</a></li>
+                <li><a href="/pages/1-currencies/binance/">Binance Coin</a></li>
             </ul>
         </div>
         <div class="footer-column">
             <h2>Online Casinos</h2>
             <ul>
-                <li><a href="/pages/2-best casino/new-casino.html">New Online Casinos</a></li>
-                
-                <li><a href="/pages/2-best casino/best-mobile.html">Mobile Casinos</a></li>
-                <li><a href="pages/2-best casino/bonus.html">Casino Bonuses</a></li>
+                <li><a href="/pages/2-best casino/mobile-casinos/">Mobile Casinos</a></li>
+                <li><a href="pages/2-best casino/bonus/">Casino Bonuses</a></li>
             </ul>
         </div>
         <div class="footer-column">
             <h2>Payment Methods</h2>
             <ul>
-                <li><a href="/pages/3-payment/mastercard.html">Mastercard casinos</a></li>
-                <li><a href="/pages/3-payment/visa.html">Visa Casinos</a></li>
-                <li><a href="/pages/3-payment/netteler.html">Neteller</a></li>
-                <li><a href="/pages/3-payment/skrill.html">Skrill</a></li>
+                <li><a href="/pages/3-payment/mastercard/">Mastercard casinos</a></li>
+                <li><a href="/pages/3-payment/visa/">Visa Casinos</a></li>
+                <li><a href="/pages/3-payment/neteller/">Neteller</a></li>
+                <li><a href="/pages/3-payment/skrill/">Skrill</a></li>
             </ul>
         </div>
         <div class="footer-column">
@@ -53,14 +52,8 @@ function loadFooter() {
                 <li><a href="/pages/slot/">Slots</a></li>
             </ul>
         </div>
-        <div class="footer-column">
-            <h2>Providers</h2>
-            <ul>
-                <li><a href="/pages/4-provider/micro.html">Microgaming</a></li>
-                <li><a href="/pages/4-provider/booongo.html">Booongo</a></li>
-                <li><a href="/pages/4-provider/habanero.html"></a>Habanero</li>
-            </ul>
-        </div>
+        <div class="footer-column"></div>
+        
     </div>
 </body>
 
